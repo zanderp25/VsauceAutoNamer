926c8e7c15d1fd45f88d365f82e6f32577d5f932
zanderp25 <zanderpicon@outlook.com>
2021-11-19 13:50:02 -0500

    minor impovements

--- bot.py
+++ bot.py
@@ -6,38 +6,44 @@ token = "Nzg0MTg4NzkwNjc4ODgwMzI2.X8lquQ.KE81OC4cNtuTkQ7kpiBztpKFX-g"
 bot = commands.Bot(command_prefix = commands.when_mentioned_or("*"), intents=intents)
 n = "\n"
 
+channel = None
+guild = None
+
 @bot.event
 async def on_ready():
     print("ready")
+    global guild, channel
+    channel = bot.get_channel(500785463624335392)
+    guild = channel.guild
 
 @bot.event
 async def on_member_join(member):
     try:
         await member.edit(nick=await get_next())
-        await bot.guilds[0].get_channel(500785463624335392).send(
+        await channel.send(
             embed = discord.Embed(
                 title = f"Welcome, {member.display_name}!",
                 color = discord.Color.green()
             )
         )
     except Exception:
-        await bot.guilds[0].get_channel(500785463624335392).send(
+        await channel.send(
             embed = discord.Embed(
                 description = "Unfortunately, I could not change the nick name of this user...",
                 color = discord.Color.red()
             )
         )
-        await bot.guilds[0].get_member(511655498676699136).send(f"Error! ```{traceback.format_exc()}```")
+        await guild.get_member(511655498676699136).send(f"Error! ```{traceback.format_exc()}```")
 
 async def get_next():
-    x = [m.nick for m in bot.guilds[0].members if not m.bot and re.match(r'^Vsauce([1-9]?[0-9]{2,})$',m.display_name)]
+    x = [m.nick for m in guild.members if not m.bot and re.match(r'^Vsauce([1-9]?[0-9]{2,})$',m.display_name)]
     x.sort()
     o = ""
-    for y in range(10, len(x)+1):
-        if f'Vsauce{y}' not in x:
-            o = f"Vsauce{y}"
+    for y in range(1, len(x)+1):
+        if f'Vsauce{str(y).zfill(2)}' not in x:
+            o = f"Vsauce{str(y).zfill(2)}"
             break
-        o = f"Vsauce{y + 1}"
+        o = f"Vsauce{str(y + 1).zfill(2)}"
     return o
 
 @bot.command()
