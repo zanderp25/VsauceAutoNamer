a87c342c39145a48c56d93de42020fb94e33c1ee
zanderp25 <zanderp@staff.clicksminuteper.net>
2022-03-10 19:08:12 -0500

    made a paginator for VAN

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
+import asyncio
 import discord, traceback, re
 from discord.colour import Color
-intents = discord.Intents(messages=True, members=True, guilds=True)
+intents = discord.Intents(messages=True, members=True, guilds=True, reactions=True, message_content=True)
 from discord.ext import commands
 token = "Nzg0MTg4NzkwNjc4ODgwMzI2.X8lquQ.KE81OC4cNtuTkQ7kpiBztpKFX-g"
 bot = commands.Bot(command_prefix = commands.when_mentioned_or("*"), intents=intents)
@@ -29,7 +30,7 @@ async def on_member_join(member):
     except Exception:
         await channel.send(
             embed = discord.Embed(
-                description = "Unfortunately, I could not change the nick name of this user...",
+                description = "Unfortunately, I could not change the nickname of this user...",
                 color = discord.Color.red()
             )
         )
@@ -65,7 +66,36 @@ async def listmembers(ctx):
     try:
         x = [f"{m.nick} (`{m}`)" for m in bot.guilds[0].members if not m.bot]
         x.sort()
-        await ctx.send(embed = discord.Embed(title = "List of members", description = n.join(x) + n + f"Number of members: {len(x)}", color = discord.Color.greyple()))
+        msg:discord.Message = await ctx.send(
+            embed = discord.Embed(
+                title = "List of members", 
+                description = n.join(x[:20]),
+                color = discord.Color.greyple(),
+            ).set_footer(
+                text=f"Number of members: {len(x)}", 
+            )
+        )
+        await msg.add_reaction("◀️")
+        await msg.add_reaction("▶️")
+        index = 0
+        while True:
+            r = await bot.wait_for('reaction_add', check=lambda reaction, user: user.id == ctx.author.id and reaction.message.id == msg.id, timeout=60.0)
+            if r[0].emoji == "◀️":
+                index -= 20
+            elif r[0].emoji == "▶️":
+                index += 20
+            else:
+                continue
+            index = max(0, min(index, len(x) - 20))
+            await msg.edit(embed = discord.Embed(
+                title = "List of members",
+                description = n.join(x[index:index+20])
+            ).set_footer(
+                text=f"Number of members: {len(x)}",
+            ))
+            await msg.remove_reaction(r[0].emoji, ctx.author)
+    except asyncio.TimeoutError:
+        await msg.clear_reactions()
     except Exception as e:
         await ctx.send(embed = discord.Embed(title = "Error", description = f"I have encountered an error.```{e}```", color = discord.Color.red()))
 
